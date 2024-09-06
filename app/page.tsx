ca9150d362e5e7c7ae5506bd83b053c3327d4b04
jpaberzs <janis.paberzs18@gmail.com>
2024-09-06 22:18:09 +0300

    feat: added shadcn

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button';
+
+export default function Page() {
+  return (
+    <>
+      <h1>Hello, Next.js!</h1>
+      <Button variant="outline">Click me</Button>
+    </>
+  );
+}

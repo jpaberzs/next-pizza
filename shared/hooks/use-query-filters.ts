f60fb58d724f92ffb6040265f8499e47c8fea7ec
jpaberzs <janis.paberzs18@gmail.com>
2024-09-25 13:16:40 +0300

    fix: query of filter position

--- shared/hooks/use-query-filters.ts
+++ shared/hooks/use-query-filters.ts
@@ -1,27 +1,19 @@
-import { useEffect, useRef } from 'react';
 import { FiltersProps } from './use-filters';
 import QueryString from 'qs';
 import { usePathname, useRouter } from 'next/navigation';
 
 export const useQueryFilters = (filters: FiltersProps) => {
-  const isMounted = useRef(false);
   const pathname = usePathname();
   const router = useRouter();
 
-  useEffect(() => {
-    if (isMounted.current) {
-      const params = {
-        ...filters.prices,
-        pizzaTypes: Array.from(filters.pizzaTypes),
-        sizes: Array.from(filters.sizes),
-        ingredients: Array.from(filters.selectedIngredients),
-      };
+  const params = {
+    ...filters.prices,
+    pizzaTypes: Array.from(filters.pizzaTypes),
+    sizes: Array.from(filters.sizes),
+    ingredients: Array.from(filters.selectedIngredients),
+  };
 
-      const queryString = QueryString.stringify(params, { arrayFormat: 'comma' });
+  const queryString = QueryString.stringify(params, { arrayFormat: 'comma' });
 
-      if (pathname === '/') router.push(`?${queryString}`, { scroll: false });
-
-      isMounted.current = true;
-    }
-  }, [pathname, filters, router]);
+  if (pathname === '/') router.push(`?${queryString}`, { scroll: false });
 };
